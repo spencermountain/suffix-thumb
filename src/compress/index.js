0af9ac3cfbbe6de8e6d34c43f32a350db910ebe4
spencer kelly <spencerkelly86@gmail.com>
2021-12-24 16:39:04 -0500

    more packing

--- src/compress/index.js
+++ src/compress/index.js
@@ -1,9 +1,39 @@
 import { pack } from 'efrt'
 // import toRules from '../learn/toRules.js'
 
+// longest common prefix
+const findOverlap = (from, to) => {
+  let all = []
+  for (let i = 0; i < from.length; i += 1) {
+    if (from[i] === to[i]) {
+      all.push(from[i])
+    } else {
+      break
+    }
+  }
+  return all.join('')
+}
+
+// remove redundancies from key-val pairs
+const pressObj = function (obj) {
+  let res = {}
+  Object.keys(obj).forEach((k) => {
+    let val = obj[k]
+    let prefix = findOverlap(k, val)
+    if (prefix.length < 2) {
+      res[k] = val
+      return
+    }
+    let out = '.' + prefix.length + val.substr(prefix.length)
+    res[k] = out
+  })
+  // res = pack(res)
+  return res
+}
 const compress = function (model = {}) {
+  model.exceptions = pressObj(model.exceptions)
   // model = toRules(model)
-  // model.exceptions = pack(model.exceptions)
+  model.exceptions = pack(model.exceptions)
   return model
 }
 export default compress
--- src/learn/08-pack.js
+++ src/learn/08-pack.js
@@ -10,39 +10,9 @@ const pressRules = function (rules) {
   return byChar
 }
 
-// longest common prefix
-const findOverlap = (from, to) => {
-  let all = []
-  for (let i = 0; i < from.length; i += 1) {
-    if (from[i] === to[i]) {
-      all.push(from[i])
-    } else {
-      break
-    }
-  }
-  return all.join('')
-}
-
-// remove redundancies from key-val pairs
-const pressObj = function (obj) {
-  let res = {}
-  Object.keys(obj).forEach((k) => {
-    let val = obj[k]
-    let prefix = findOverlap(k, val)
-    if (prefix.length < 2) {
-      res[k] = val
-      return
-    }
-    let out = '.' + prefix.length + val.substr(prefix.length)
-    res[k] = out
-  })
-  // res = pack(res)
-  return res
-}
 
 const compress = function (model) {
   model.rules = pressRules(model.rules)
-  // model.exceptions = pressObj(model.exceptions)
   // console.dir(model, { depth: 5 })
   return model
 }
